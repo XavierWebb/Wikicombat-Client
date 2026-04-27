d7ec396b92b9786082e986f6e7a3303c998e51fb
kali <xaviernavarro2017@gmail.com>
2026-04-27 11:43:46 -0300

    Home Message

---
+++ src/app/components/Texts/Small_Text/small_text.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'small-text',
+    imports: [CommonModule],
+    standalone: true,
+    templateUrl: './small_text.component.html'
+})
+
+export class SmallText {}
---
+++ src/app/components/Title/Mid_Title/mid_title.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mid-title',
+    imports: [CommonModule],
+    standalone: true,
+    templateUrl: './mid_title.component.html',
+})
+
+export class MidTitle {}
